be2bc6540ad1b1cca9aa3391c02b6f046f367e5d
sanaaa15 <sanaaa.sumannn@gmail.com>
2023-11-20 12:24:24 +0530

    committed

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -4,8 +4,8 @@ import { db } from '../db.js';
 export const getClubDetails = (req, res) => {
   const { clubName } = req.params;
 
-  // Logic to fetch club details based on clubName from the database
-  const getClubDetailsQuery = "SELECT * FROM Club WHERE clubName = ?";
+ 
+  const getClubDetailsQuery = "SELECT * FROM Club WHERE clubname = ?";
   
   db.query(getClubDetailsQuery, [clubName], (err, data) => {
     if (err) {
@@ -13,7 +13,6 @@ export const getClubDetails = (req, res) => {
     }
 
     if (data.length) {
-      // Send club details as a response
       return res.status(200).json(data[0]);
     } else {
       return res.status(404).json("Club not found!");
