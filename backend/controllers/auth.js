47010b43c6a3ee893b4ba9989bb1f74a96a4c117
sanaaa15 <sanaaa.sumannn@gmail.com>
2023-11-20 12:29:14 +0530

    committed

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,9 +1,7 @@
 import { db } from "../db.js";
-import bcrypt from "bcrypt";
-
 
 export const register = (req, res) => {
-  const { email, password, userType, clubName, facultyId, headSrn, srn, name, clubType } = req.body;
+  const { email, password, userType, clubName, facultyId, headSrn, srn, name, clubType, domain, sem, dept, phoneno, gender } = req.body;
 
   const checkUserQuery = "SELECT * FROM Member WHERE email = ?";
   db.query(checkUserQuery, [email], (err, data) => {
@@ -21,8 +19,8 @@ export const register = (req, res) => {
       insertQuery = "INSERT INTO Club (`clubname`, `facultyid`, `headSRN`, `type`) VALUES (?, ?, ?, ?)";
       values = [clubName, facultyId, headSrn, clubType];
 
-      const insertQueryMember = "INSERT INTO Member (`SRN`, `name`, `email`, `password`) VALUES ( ?, ?, ?, ?)";
-      const valuesMember = [headSrn, name, email, password];
+      const insertQueryMember = "INSERT INTO Member (`SRN`, `name`, `email`, `password`, `sem`, `dept`, `phoneno`, `gender`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+      const valuesMember = [headSrn, name, email, password, sem, dept, phoneno, gender];
 
       db.query(insertQueryMember, valuesMember, (err, data) => {
         if (err) {
@@ -47,23 +45,47 @@ export const register = (req, res) => {
         });
       });
     } else if (userType === 'member') {
-      insertQuery = "INSERT INTO Member (`SRN`, `name`, `email`, `password`) VALUES (?, ?, ?, ?)";
-      values = [srn, name, email, password, clubName];
+      const checkDomainQuery = "SELECT domainid FROM Domain WHERE LOWER(domainname) = LOWER(?)";
 
-      db.query(insertQuery, values, (err, data) => {
+      db.query(checkDomainQuery, [domain], (err, domainData) => {
         if (err) {
           return res.status(500).json(err);
         }
 
-        const insertQueryClubMember = "INSERT INTO ClubMember (`SRN`, `clubname`) VALUES (?, ?)";
-        const valuesClubMember = [srn, clubName];
+        if (domainData.length === 0) {
+          return res.status(404).json("Domain not found");
+        }
 
-        db.query(insertQueryClubMember, valuesClubMember, (err, data) => {
+        const domainId = domainData[0].domainid;
+
+        insertQuery = "INSERT INTO Member (`SRN`, `name`, `email`, `password`, `sem`, `dept`, `phoneno`, `gender`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        values = [srn, name, email, password, sem, dept, phoneno, gender];
+
+        db.query(insertQuery, values, (err, data) => {
           if (err) {
             return res.status(500).json(err);
           }
 
-          return res.status(200).json("User has been created");
+          const insertQueryMemberDomain = "INSERT INTO ClubDomain (`domainid`, `SRN`, `clubname`) VALUES (?, ?, ?)";
+          const valuesMemberDomain = [domainId, srn, clubName];
+
+          db.query(insertQueryMemberDomain, valuesMemberDomain, (err, data) => {
+            if (err) {
+              return res.status(500).json(err);
+            }
+
+            return res.status(200).json("User has been created");
+          });
+          const insertQueryClubMember = "INSERT INTO ClubMember (`SRN`, `clubname`) VALUES (?, ?)";
+          const valuesClubMember = [srn, clubName];
+
+          db.query(insertQueryClubMember, valuesClubMember, (err, data) => {
+            if (err) {
+              return res.status(500).json(err);
+            }
+
+            return res.status(200).json("Club has been created");
+          });
         });
       });
     } else {
@@ -71,52 +93,42 @@ export const register = (req, res) => {
     }
   });
 };
-
-
-
-
-
-
-
-
-
-
+ 
 
 export const login = (req, res) => {
   const { srn, password, userType, clubName } = req.body;
 
   if (userType === 'clubHead') {
-    // Club Head login logic
     const checkClubHeadQuery = `
-  SELECT M.password as  clubPassword, C.*
-  FROM Member M
-  JOIN Club C ON M.SRN = C.headid
-  WHERE C.clubName = ?`;
+      SELECT M.password as clubPassword, C.*
+      FROM Member M
+      JOIN Club C ON M.SRN = C.headSRN
+      WHERE C.clubname = ?`;
 
-db.query(checkClubHeadQuery, [clubName], (err, data) => {
-  if (err) {
-    return res.status(500).json(err);
-  }
+    db.query(checkClubHeadQuery, [clubName], (err, data) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
 
-  if (data.length) {
-    const hashedPassword = data[0].clubPassword;
+      if (data.length) {
+        const storedPassword = data[0].clubPassword;
 
-    if (bcrypt.compareSync(password, hashedPassword)) {
-      return res.status(200).json({
-        message: "Club Head Logged in!",
-        userType: 'clubHead',
-        clubName: data[0].clubName,
-      });
-    } else {
-      return res.status(400).json("Wrong Club Name/Password!");
-    }
-  } else {
-    return res.status(404).json("Club not found or wrong Club Name/Password!");
-  }
-});
+        if (password === storedPassword) {
+          return res.status(200).json({
+            message: "Club Head Logged in!",
+            userType: 'clubHead',
+            clubName: data[0].clubName,
+          });
+        } else {
+          return res.status(400).json("Wrong Club Name/Password!");
+        }
+      } else {
+        return res.status(404).json("Club not found or wrong Club Name/Password!");
+      }
+    });
 
   } else if (userType === 'member') {
-    // Regular Member login logic
+   
     const checkMemberQuery = "SELECT * FROM Member WHERE SRN = ?";
     db.query(checkMemberQuery, [srn], (err, data) => {
       if (err) {
@@ -124,14 +136,17 @@ db.query(checkClubHeadQuery, [clubName], (err, data) => {
       }
 
       if (data.length) {
-        const result = bcrypt.compareSync(password, data[0].password);
-        if (!result) return res.status(400).json("Wrong SRN/Password!");
+        const storedPassword = data[0].password;
 
-        return res.status(200).json({
-          message: "Member Logged in!",
-          userType: 'member',
-          SRN: data[0].SRN,
-        });
+        if (password === storedPassword) {
+          return res.status(200).json({
+            message: "Member Logged in!",
+            userType: 'member',
+            SRN: data[0].SRN,
+          });
+        } else {
+          return res.status(400).json("Wrong SRN/Password!");
+        }
       } else {
         return res.status(404).json("Member not found or wrong SRN/Password!");
       }
