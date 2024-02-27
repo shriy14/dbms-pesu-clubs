03d66bab67fbc05ee2fbeaa6dbfd8386e303fa05
Sana Suman <106922110+sanaaa15@users.noreply.github.com>
2024-02-27 12:48:50 +0530

    commit

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "@Sana121",
+  password: "your-db-password",
   database: "pesu", 
 });
 
